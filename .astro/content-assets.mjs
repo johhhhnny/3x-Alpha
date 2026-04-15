194c50bbb0ca4abcacae93e3aa1f1776b6a8c431
johhhhnny <johnny240926@outlook.com>
2026-04-15 23:33:30 +0800

    Refactor code structure for improved readability and maintainability

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,6 @@
 
 import __ASTRO_IMAGE_IMPORT_Z1j92dj from "@assets/images/articles/naval-how-to-get-rich/cover.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Farticles%2Fnaval-how-to-get-rich%2Findex.mdx";
-export default new Map([["@assets/images/articles/naval-how-to-get-rich/cover.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Farticles%2Fnaval-how-to-get-rich%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z1j92dj]]);
+import __ASTRO_IMAGE_IMPORT_Z1zvoUo from "@assets/images/articles/naval-how-to-get-rich/cover.png?astroContentImageFlag=&importer=src%2Fcontent%2Farticles%2Fnaval-how-to-get-rich%2Findex.mdx";
+import __ASTRO_IMAGE_IMPORT_7Xv3l from "@assets/images/authors/3x-alpha/avatar.png?astroContentImageFlag=&importer=src%2Fcontent%2Fauthors%2F3x-alpha%2Findex.mdx";
+export default new Map([["@assets/images/articles/naval-how-to-get-rich/cover.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Farticles%2Fnaval-how-to-get-rich%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z1j92dj], ["@assets/images/articles/naval-how-to-get-rich/cover.png?astroContentImageFlag=&importer=src%2Fcontent%2Farticles%2Fnaval-how-to-get-rich%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Z1zvoUo], ["@assets/images/authors/3x-alpha/avatar.png?astroContentImageFlag=&importer=src%2Fcontent%2Fauthors%2F3x-alpha%2Findex.mdx", __ASTRO_IMAGE_IMPORT_7Xv3l]]);
 		
